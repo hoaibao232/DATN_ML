0955aa1e19570d00bcf642e4a345cda07fc211b3
hoaibao232 <nguyenhoaibaoltt@gmail.com>
2022-01-04 12:00:34 +0700

    fix to deploy

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from typing import TypedDict
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from tkinter import *
 from sklearn.cluster import KMeans
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
